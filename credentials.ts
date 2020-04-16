21722e07a97229e04abda5452009d6396df1157e
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2020-04-16 08:26:21 +0200

    test creds removed from .gitignore

---
+++ credentials.ts
@@ -0,0 +1,13 @@
+
+export const firebaseConfig = {
+
+    firebase: {
+        apiKey: 'AIzaSyBsz0Xdf7cJffF-WfK-r2SIgN4GE04MPNU',
+        authDomain: 'candychase2020-ionic5.firebaseapp.com',
+        databaseURL: 'https://candychase2020-ionic5.firebaseio.com',
+        projectId: 'candychase2020-ionic5',
+        storageBucket: 'candychase2020-ionic5.appspot.com',
+        messagingSenderId: '743398085439'
+    }
+
+};
