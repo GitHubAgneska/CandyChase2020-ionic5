bf8447bb0efbb537d8397e172802c1249c43b765
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2020-06-07 00:31:21 +0200

    fix missing goBack() method in login component

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {}
+
+  public goBack() {
+    this.location.back();
+  }
 
 }
