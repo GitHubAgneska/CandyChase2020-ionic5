20da86d85b0aca3466b42dbb0613902a1d89b179
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2020-06-15 17:28:44 +0200

    fix candylist loading / login page add note / addresses list data retrieving (non formatted yet)

--- src/app/play/addresses/addresses.component.ts
+++ src/app/play/addresses/addresses.component.ts
@@ -16,12 +16,16 @@ export class AddressesComponent implements OnInit {
   public address: any = {};
   public addressCoords: any = {};
 
+  public noAddressYet: boolean;
+  public noAddressBubble = 'assets/graphicMat/noAddressYet_bubble.png';
+
 
   constructor(
     private userStatsService: UserStatsService
   ) {
     this.addressList = [];
     this.addressCoords = { lat: 0, long: 0 };
+    this.noAddressYet = true;
   }
 
   ngOnInit() {
@@ -29,7 +33,9 @@ export class AddressesComponent implements OnInit {
     this.userStatsService.getCurrentAddressesList().subscribe(data => {
       this.addressList.push(data);
       console.log('ADDRESSLIST PUSH==', this.addressList);
+      if (this.addressList.length === 0 ) {
+        this.noAddressYet = true;
+      }
     });
   }
-
 }
